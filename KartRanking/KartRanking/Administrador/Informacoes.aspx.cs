0bc637525fd1b6ac6f37cbd814c96769bfb7c5b3
afonsoft <SND\afonsoft_cp>
2014-03-18 20:44:36 +0000

    Informações na tela de cadastro.

---
+++ KartRanking/KartRanking/Administrador/Informacoes.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using KartRanking.Page;
+
+namespace KartRanking.Administrador
+{
+    public partial class Informacoes : PageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ((Admin)Page.Master).ConteudoVisible = true;
+            }
+        }
+    }
+}
