2ba73ec364d941b35950be660c89ddb1dd44156b
Afonso Dutra Nogueira Filho <afonsoft@gmail.com>
2014-03-26 16:06:16 +0000

    Tela de informações / Regras do grupo.

--- KartRanking/Grupo/informacoes.aspx.cs
+++ KartRanking/Grupo/informacoes.aspx.cs
@@ -9,18 +9,33 @@ using KartRanking.Page;
 namespace KartRanking.Grupo
 {
     /********************************************************
-    * CRIADO POR: AFONSO DUTRA NOGUEIRA FILHO (AFONSOFT)   *
-    * DATA: 26-03-2014                                     *
-    * E-MAIL: afonsoft@outlook.com.br                      *
-    * ******************************************************
-    * ALTERAÇÕES:                                          *
-    *                                                      *
-    ********************************************************/
+    * CRIADO POR: AFONSO DUTRA NOGUEIRA FILHO (AFONSOFT)    *
+    * DATA: 26-03-2014                                      *
+    * E-MAIL: afonsoft@outlook.com.br                       *
+    * *******************************************************
+    * ALTERAÇÕES:                                           *
+    * Issues: 1759 - Criação da Tela de informações da Regras
+    *********************************************************/
     public partial class informacoes : PageBaseGrupo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                PopularRegras();
+            }
+        }
 
+        private void PopularRegras()
+        {
+            ExibirRegras.InnerHtml = "";
+
+            string regra = (from r in dk.Kart_Regras_Grupos
+                            where r.idGrupo == IdGrupo
+                            && r.Ativo == true
+                            select r.Regra).FirstOrDefault();
+
+            ExibirRegras.InnerHtml = regra;
         }
     }
 }
