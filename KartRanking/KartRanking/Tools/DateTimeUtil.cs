07169f18781d1a8360c5a4a14e2d35d53b0bba73
afonsoft <SND\afonsoft_cp>
2014-05-14 18:28:10 +0000

    Criação do metodo para chave unica e uma pagina para atualizar os usuário no forum.

--- KartRanking/KartRanking/Tools/DateTimeUtil.cs
+++ KartRanking/KartRanking/Tools/DateTimeUtil.cs
@@ -5,6 +5,9 @@ using System.Web;
 
 namespace KartRanking.Tools
 {
+    /// <summary>
+    /// Classe para trabalhar com Datetime
+    /// </summary>
     public static class DateTimeUtil
     {
         /// <summary>
@@ -21,6 +24,54 @@ namespace KartRanking.Tools
             int Sm = Convert.ToInt16(Hr[2]) * 1000;
 
             return hm + Mm + Sm;
+        }
+    }
+
+    /// <summary>
+    /// Classe para gerar chave unica.
+    /// </summary>
+    public static class UniqueKey
+    {
+        /// <summary>
+        /// Gera uma chave unica utilizando o RNGCryptoServiceProvider
+        /// </summary>
+        public static string GetUniqueKey(int Size)
+        {
+            return GetUniqueKey(Size, true, true);
+        }
+        /// <summary>
+        /// Gera uma chave unica utilizando o RNGCryptoServiceProvider
+        /// </summary>
+        public static string GetUniqueKey(int Size, bool OnlyUpper)
+        {
+            return GetUniqueKey(Size, OnlyUpper, false);
+        }
+
+        /// <summary>
+        /// Gera uma chave unica utilizando o RNGCryptoServiceProvider
+        /// </summary>
+        public static string GetUniqueKey(int Size, bool OnlyUpper, bool RemoveZero)
+        {
+            //Remover o Zero 0 e o "O"
+            string letters = "";
+            if(OnlyUpper && RemoveZero)
+                letters = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+            else if(!OnlyUpper && RemoveZero)
+                letters = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz";
+            else if (OnlyUpper && !RemoveZero)
+                letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            else if (!OnlyUpper && !RemoveZero)
+                letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+            char[] chars = letters.ToCharArray(); 
+            byte[] data = new byte[Size];
+            System.Text.StringBuilder result = new System.Text.StringBuilder(Size);
+            System.Security.Cryptography.RNGCryptoServiceProvider crypto = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+            foreach (byte b in data)
+                result.Append(chars[b % (chars.Length - 1)]);
+
+            return result.ToString().ToUpper();
         }
     }
 }
